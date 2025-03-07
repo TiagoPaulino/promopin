2f29306daad15d32fa14868548057ecc1b86853f
TiagoPaulino <tiago.paulino96@gmail.com>
2025-03-07 14:01:21 -0300

    Ajuste na lista de produtos

--- app/components/productList.tsx
+++ app/components/productList.tsx
@@ -1,19 +1,27 @@
-
 import axios from "axios";
 import { Product } from "@/app/models/product";
-import {ProductCard} from "./productCard";
-import { env } from "process";
+import { ProductCard } from "./productCard";
 
 export async function ProductList() {
-    const apiUrl = env.NEXT_PUBLIC_API_URL
-    const productList: { data: Product[] } =  await axios.get(`${apiUrl}/api/products/list`);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
+    if (!apiUrl) {
+        console.error("NEXT_PUBLIC_API_URL não está definida!");
+        return <p></p>;
+    }
 
-    return (
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-3 md:max-w-1/2 mx-auto">
-            {productList.data.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
-    )
+    try {
+        const { data }: { data: Product[] } = await axios.get(`${apiUrl}/api/products/list`);
+
+        return (
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-3 md:max-w-1/2 mx-auto">
+                {data.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        );
+    } catch (error) {
+        console.error("Erro ao buscar produtos:", error);
+        return <p></p>;
+    }
 }
