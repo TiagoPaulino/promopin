2ed8e295f841ea771b3c7164fd9101d1e7a9db40
TiagoPaulino <tiago.paulino96@gmail.com>
2025-03-06 21:58:03 -0300

    Atualizado Cors polices

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,36 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'down-br.img.susercontent.com',
-        port: '',
-        pathname: '/file/**',
-        search: '',
+        protocol: "https",
+        hostname: "down-br.img.susercontent.com",
+        port: "",
+        pathname: "/file/**",
+        search: "",
       },
     ],
   },
-  /* config options here */
+
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)", // Aplica a todas as rotas da API
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "chrome-extension://fjkenfedaihomanollgdkblkbglfdbnl", // ID da sua extensão
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
