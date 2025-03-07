2f54f84a7ffc75cd5c78af0e0c1536184fe9864f
TiagoPaulino <tiago.paulino96@gmail.com>
2025-03-06 22:31:37 -0300

    Cors polices

--- pages/api/products/create.ts
+++ pages/api/products/create.ts
@@ -3,9 +3,9 @@ import prisma from '@/lib/prisma';
 import { Product } from '@/app/models/product';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
+  // if (req.method === 'OPTIONS') {
+  //   return res.status(200).end();
+  // }
 
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
